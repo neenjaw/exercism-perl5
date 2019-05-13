acf13d604952bd1b00e0aad898a9d165f3f8976c
Tim Austin <tim@neenjaw.com>
2019-05-13 10:45:19 -0600

    leap revision

--- leap/Leap.pm
+++ leap/Leap.pm
@@ -8,17 +8,27 @@ our @EXPORT_OK = qw(is_leap_year);
 sub is_leap_year {
   my ($year) = @_;
 
+  # Method 1) Multiple return points
+
   # There is no year 0, not a leap year
-  return 0 if $year == 0;
+  # return 0 if $year == 0;
 
   # If the year is not evenly divisible by 4, not a leap year
-  return 0 if ($year % 4) != 0;
+  # return 0 if ($year % 4) != 0;
 
   # If the year is evenly divisible by 100 but not 400, not a leap year
-  return 0 if (($year % 100) == 0) and (($year % 400) != 0);
+  # return 0 if (($year % 100) == 0) and (($year % 400) != 0);
 
   # the year is a leap year
-  return 1;
+  # return 1;
+
+  # Method 2) Single return point
+
+  return (
+    ($year != 0) 
+    and ($year % 4 == 0) 
+    and ((not ($year % 100 == 0)) or ($year % 400 == 0))
+  );
 }
 
 1;
