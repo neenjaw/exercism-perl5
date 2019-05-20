6b95fdd5ec136e5d3fee7cf64cfbf897a86888f9
Tim Austin <tim@neenjaw.com>
2019-05-19 23:40:12 -0600

    raindrops soln

---
+++ raindrops/Raindrops.pm
@@ -0,0 +1,35 @@
+package Raindrops;
+use strict;
+use warnings;
+use Exporter 'import';
+our @EXPORT_OK = qw(raindrop);
+
+sub raindrop {
+  my ($number) = @_;
+
+  my @factor_sounds = (
+    [3, 'Pling'], 
+    [5, 'Plang'], 
+    [7, 'Plong']
+    );
+
+  my $result = '';
+
+  for my $factor_sound (@factor_sounds) {
+    my ($factor, $sound) = @$factor_sound;
+
+    if (is_factor($number, $factor)) { 
+      $result .= $sound
+    }
+  }
+  
+  return $result || $number;
+}
+
+sub is_factor {
+  my ($number, $factor) = @_;
+
+  return $number % $factor == 0;
+}
+
+1;
