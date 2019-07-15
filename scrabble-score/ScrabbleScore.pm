b70fef993624054f52239d2e5f3f0df232815bbf
Tim Austin <tim@neenjaw.com>
2019-07-15 00:38:44 -0600

    generates hash map

--- scrabble-score/ScrabbleScore.pm
+++ scrabble-score/ScrabbleScore.pm
@@ -34,34 +34,17 @@ sub score {
 }
 
 sub score_map {
-  return {
-    'A' => 1, 
-    'E' => 1, 
-    'I' => 1, 
-    'O' => 1, 
-    'U' => 1, 
-    'L' => 1, 
-    'N' => 1, 
-    'R' => 1, 
-    'S' => 1, 
-    'T' => 1, 
-    'D' => 2, 
-    'G' => 2, 
-    'B' => 3, 
-    'C' => 3, 
-    'M' => 3, 
-    'P' => 3,
-    'F' => 4, 
-    'H' => 4, 
-    'V' => 4, 
-    'W' => 4, 
-    'Y' => 4,
-    'K' => 5,
-    'J' => 8, 
-    'X' => 8, 
-    'Q' => 10, 
-    'Z' => 10
-  };
+  my %hash;
+
+  $hash{$_} = 1  for qw(A E I O U L N R S T);
+  $hash{$_} = 2  for qw(D G);
+  $hash{$_} = 3  for qw(B C M P);
+  $hash{$_} = 4  for qw(F H V W Y);
+  $hash{$_} = 5  for qw(K);
+  $hash{$_} = 8  for qw(J X);
+  $hash{$_} = 10 for qw(Q Z);
+
+  return \%hash;
 }
 
 1;
